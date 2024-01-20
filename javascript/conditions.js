404d91ed90f8a2f4422b2b2474a45a40eb2bd003
ssimsir <s_simsir@yaho.com>
2024-01-20 12:56:18 +0300

    4. finished

--- javascript/conditions.js
+++ javascript/conditions.js
@@ -1,12 +1,64 @@
 
 
-console.log("conditions");
+// * ============================================
+// *               KARAR YAPILARI
+// * ============================================
 
-//const num1=+prompt("bir sayı giriniz");    ///Genelde Number fonksiyonu kullanılır
-const num1=Number(prompt("bir sayı giriniz"));
-const num2=5;
+console.log("*** CONDITIONS ***")
 
-console.log(num1, typeof num1);
-console.log("Toplam", num1+num2);   //concatination   String birleştirme
 
-//toplama işleminde bir tane string varsa concatination yapılır
+//!Örnek 1
+// const num1 = +prompt("Bir sayi giriniz:")
+//const num1 = Number(prompt("Bir sayi giriniz:"))
+const num1 = 5
+// console.log(num1, typeof num1)
+// console.log("Toplam:", num1 + num2) //? String birleştirme (concatination)
+
+if (num1 < 0) {
+  console.log("Girilen sayi negatiftir")
+} else if (num1 > 0) {
+  console.log("Girilen sayi pozitiftir")
+} else {
+  console.log("Girilen sayi 0'a eşittir")
+}
+
+//!Örnek 2 girilen sayının en büyüğü
+
+const  sayi1=NaN
+const  sayi2=undefined
+const  sayi3=null
+
+//? 1. yöntem build in fonksiyon yardımıyla
+console.log(Math.max(sayi1, sayi2, sayi3))   
+
+//? 2. yöntem nested if
+if (sayi1>sayi2){
+    if (sayi1>sayi3){
+        console.log("Enbüyük sayi ", sayi1)
+    }
+}
+
+if (sayi2>sayi1){
+    if (sayi2>sayi3){
+        console.log("Enbüyük sayi ", sayi2)
+    }
+}
+
+if (sayi3>sayi2){
+    if (sayi3>sayi1){
+        console.log("Enbüyük sayi ", sayi3)
+    }
+}
+
+//? 3. yöntem logic operatorler
+
+if (sayi1>=sayi2 && sayi1>=sayi3){
+    console.log("En büyük sayi", sayi1)
+} else if (sayi2>=sayi1 && sayi2>=sayi3){
+    console.log("En büyük sayi", sayi2)
+} else if (sayi3>=sayi1 && sayi3>=sayi2){
+    console.log("En büyük sayi", sayi3)
+} else {
+    console.log("yanlış işlem")
+}
+
