bc2e08006a751c60066248e00cfef69afce28fb7
ssimsir <s_simsir@yaho.com>
2024-01-20 12:03:25 +0300

    conditions started

---
+++ javascript/conditions.js
@@ -0,0 +1,12 @@
+
+
+console.log("conditions");
+
+//const num1=+prompt("bir sayı giriniz");    ///Genelde Number fonksiyonu kullanılır
+const num1=Number(prompt("bir sayı giriniz"));
+const num2=5;
+
+console.log(num1, typeof num1);
+console.log("Toplam", num1+num2);   //concatination   String birleştirme
+
+//toplama işleminde bir tane string varsa concatination yapılır
