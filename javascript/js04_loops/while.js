d4d58672b78148140841c2cadf7cf09f6cf23d50
ssimsir <s_simsir@yaho.com>
2024-01-30 00:44:19 +0300

    calculator finished

---
+++ javascript/js04_loops/while.js
@@ -0,0 +1,12 @@
+// * ============================================
+// *            DONGULER(LOOPS)- WHILE
+// * ============================================
+
+console.log("****** WHILE *******")
+
+let i=1;
+while(i<10){
+    console.log(i, "chort 16")
+    i++;
+}
+console.log("döngü bitti")
