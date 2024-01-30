e2f9399a18cb879bdcd41fb7b453f72872a2e3da
ssimsir <s_simsir@yaho.com>
2024-01-31 01:01:05 +0300

    guess game finished

--- javascript/js05_funtions1/declaration.js
+++ javascript/js05_funtions1/declaration.js
@@ -6,3 +6,12 @@ console.log("** FUNC DECLARATION **")
 
 //* ORNEK1:
 //*******************************/
+
+let number=Number(prompt("Bir sayı giriniz"));
+
+
+if (!isNaN(number)){
+    console.log(number);
+}else{
+    console.log("numara ifade giriniz");
+}
