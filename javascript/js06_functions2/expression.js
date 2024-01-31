5e6bacb0cdc547ab75a03c817d192c9c05ea1b6d
ssimsir <s_simsir@yahoo.com>
2024-01-31 20:57:41 +0300

    expression usAl finished

--- javascript/js06_functions2/expression.js
+++ javascript/js06_functions2/expression.js
@@ -4,6 +4,11 @@
 
 console.log("*** 2-EXPRESSION ****")
 
+
+// ? ======================================
+// ? Example 1
+// ? ======================================
+
 const tekVeyaCift = function(num){
 
     return num%2===0 ?  `${num} sayısı çifttir` : `${num} sayısı tektir`
@@ -12,3 +17,18 @@ const tekVeyaCift = function(num){
 
 const sonuc = tekVeyaCift(5)
 console.log(sonuc)
+
+
+
+
+
+// ? ======================================
+// ? Example 2
+// ? ======================================
+
+const usAl = function(taban, us){
+    return taban ** us;
+}
+
+const us = usAl(2,4)
+console.log(us)
