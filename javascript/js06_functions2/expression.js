1ebef3d8ccacd05878657cf12a498e157257d668
ssimsir <s_simsir@yahoo.com>
2024-01-31 20:50:34 +0300

    even odd finished

--- javascript/js06_functions2/expression.js
+++ javascript/js06_functions2/expression.js
@@ -3,3 +3,12 @@
 // ? ======================================
 
 console.log("*** 2-EXPRESSION ****")
+
+const tekVeyaCift = function(num){
+
+    return num%2===0 ?  `${num} sayısı çifttir` : `${num} sayısı tektir`
+
+}
+
+const sonuc = tekVeyaCift(5)
+console.log(sonuc)
