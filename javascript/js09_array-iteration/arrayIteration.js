fb29285fa794c9c65b41e3be22e215d7395adb77
ssimsir <s_simsir@yahoo.com>
2024-02-13 09:22:51 +0300

    css animation started

--- javascript/js09_array-iteration/arrayIteration.js
+++ javascript/js09_array-iteration/arrayIteration.js
@@ -2,30 +2,26 @@
 //*           FOREACH METHOD
 //* =======================================
 
-console.log("****** ITERATION **********")
+console.log("****** ITERATION **********");
 
-const sayilar=[4, 5, 1,4,6]
+const sayilar = [4, 5, 1, 4, 6];
 
-sayilar.forEach((s) => console.log(s))
+sayilar.forEach((s) => console.log(s));
 
-const sayilar1=[4, 5, 1,4,6]
+const sayilar1 = [4, 5, 1, 4, 6];
 
-let sum=0;
-sayilar1.forEach((sayi)=>sum += sayi)
-console.log(sum)
+let sum = 0;
+sayilar1.forEach((sayi) => (sum += sayi));
+console.log(sum);
 
-const notlar=[40, 50, 10,40,60,90]
-notlar.forEach((not, indis, array)=>{
+const notlar = [40, 50, 10, 40, 60, 90];
+notlar.forEach((not, indis, array) => {
+  array[indis] = not * 1.1;
+});
 
-    array[indis]=not*1.1
-})
+console.log(notlar);
 
-console.log(notlar)
-
-/* forEach  return yapmaz void functiondur*/ M
-
-
-
+/* forEach  return yapmaz void functiondur*/ M;
 
 /*----------------------- MAP Method------------------------*/
 
@@ -35,23 +31,31 @@ console.log(notlar)
 // const copiedNames =  names.map((n)=> n.toLowerCase())
 // console.log(copiedNames)
 
-
-
 const euro = 32.97;
 const dolar = 30.5;
 
-const iPhoneTl = [90000, 75000, 60000, 40000, 30000] 
+const iPhoneTl = [90000, 75000, 60000, 40000, 30000];
 
-const iPhoneDolar = iPhoneTl.map((tl)=> Math.trunc(tl/dolar) )
+const iPhoneDolar = iPhoneTl.map((tl) => Math.trunc(tl / dolar));
 
-const iPhoneEuro = iPhoneTl.map((tl)=> Math.trunc(tl/euro) )
-console.log(iPhoneDolar)
-console.log(iPhoneEuro)
+const iPhoneEuro = iPhoneTl.map((tl) => Math.trunc(tl / euro));
+console.log(iPhoneDolar);
+console.log(iPhoneEuro);
 
+const salary = [90000, 75000, 60000, 40000, 30000];
 
+zamliMaas = salary.filter((m) => m < 50000).map((m) => m * 1.2);
+console.log(zamliMaas);
 
+/*      REDUCE      */
 
-const salary = [90000, 75000, 60000, 40000, 30000] 
- 
-zamliMaas =  salary.filter((m)=>m<50000).map((m)=>m*1.2)
-console.log(zamliMaas)
+const maaslar = [40000, 30000, 20000, 10000];
+const toplamMaas = maaslar.reduce((t, m) => t + m);
+console.log(toplamMaas);
+
+const toplamZamlıMaas = maaslar
+  .filter((m) => m < 40000)
+  .map((m) => m * 1.5)
+  .reduce((acc, val) => acc + val, 0);
+
+console.log(toplamZamlıMaas);
