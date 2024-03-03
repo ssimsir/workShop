9ee926102e15dc3668b09066f26f94ddb0819827
ssimsir <s_simsir@yahoo.com>
2024-03-03 19:09:59 +0300

    proje kontrol

--- teamwork/SorularAlisverisSepeti/app.js
+++ teamwork/SorularAlisverisSepeti/app.js
@@ -1,35 +1,35 @@
 const urunler =  [
     {
-      "id": 1,
-      "ad": "Akıllı Telefon",
-      "marka": "Xiaomi",
-      "model": "Redmi Note 12 Pro",
-      "renk": "Siyah",
-      "fiyat": 1999.99,
-      "stok": 50
+      id: 1,
+      ad: "Akıllı Telefon",
+      marka: "Xiaomi",
+      model: "Redmi Note 12 Pro",
+      renk: "Siyah",
+      fiyat: 1999.99,
+      stok: 50
     },
     {
-      "id": 2,
-      "ad": "Dizüstü Bilgisayar",
-      "marka": "Casper",
-      "model": "Nirvana C370.4020-4C00B1",
-      "renk": "Gri",
-      "fiyat": 2999.99,
-      "stok": 25
+      id: 2,
+      ad: "Dizüstü Bilgisayar",
+      marka: "Casper",
+      model: "Nirvana C370.4020-4C00B1",
+      renk: "Gri",
+      fiyat: 2999.99,
+      stok: 25
     },
     {
-      "id": 3,
-      "ad": "Kablosuz Kulaklık",
-      "marka": "MATEO",
-      "model": "Kablosuz 5.0 Bluetooth P9 Uyumlu Kablosuz Kulaklık",
-      "renk": "Beyaz",
-      "fiyat": 149.99,
-      "stok": 100
+      id: 3,
+      ad: "Kablosuz Kulaklık",
+      marka: "MATEO",
+      model: "Kablosuz 5.0 Bluetooth P9 Uyumlu Kablosuz Kulaklık",
+      renk: "Beyaz",
+      fiyat: 149.99,
+      stok: 100
     }
   ]
 
-  sepet=[];
-  sepeteEkle = (urunId, miktar) => {
+  let sepet=[];
+  sepeteEkle = (urunler, urunId, miktar) => {
     urunler.map((urun)=>{
         if (urun.id === urunId) {
             sepet.push({id:urun.id, ad:urun.ad, marka:urun.marka, model:urun.model, renk:urun.renk, fiyat:urun.fiyat, miktar:miktar})
@@ -37,9 +37,9 @@ const urunler =  [
         }
     })
   }
-
-  sepeteEkle(1, 2)
-  sepeteEkle(2, 2)
+  
+  sepeteEkle(urunler,2, 2)
+  sepeteEkle(urunler,3, 5)
   console.log(sepet)
   console.log(urunler)
 
