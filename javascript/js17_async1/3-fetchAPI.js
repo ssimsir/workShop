724cc0a5bf6fe8373c068e0f2addfaae9d2c647b
ssimsir <s_simsir@yahoo.com>
2024-03-07 20:30:23 +0300

    async away

--- javascript/js17_async1/3-fetchAPI.js
+++ javascript/js17_async1/3-fetchAPI.js
@@ -11,3 +11,47 @@
 //? bir parametre almaktadir ve bu istegin cevabini gosteren bir Promise dondurmektedir.
 
 console.log("FETCH")
+
+
+
+const tablodaGoster = (kitaplarObject, bindElement) => {
+    tablo = document.createElement("table");
+  
+    tablo.classList.add("table", "table-striped");
+  
+  
+    //TABLO HEADER
+    const tabloBaslik = tablo.createTHead();
+    const tabloBaslikRow = tabloBaslik.insertRow();
+    const basliklar = Object.keys(kitaplarObject[0]);
+    for (let baslik of basliklar) {
+      let tabloBaslikText = document.createTextNode(baslik.toUpperCase());
+      let tabloBaslikTH = document.createElement("th");
+      tabloBaslikTH.appendChild(tabloBaslikText);
+      tabloBaslikRow.appendChild(tabloBaslikTH);
+    }
+    //TABLO BODY
+    const tabloBody = tablo.createTBody();
+    kitaplarObject.forEach((element) => {
+      let tabloRow = tabloBody.insertRow();
+      for (key in element) {
+        let tabloCell = tabloRow.insertCell();
+        let tabloIcerik = document.createTextNode(element[key]);
+        tabloCell.appendChild(tabloIcerik);
+      }
+    });
+  
+    const tabloDiv = document.querySelector(bindElement);
+    tabloDiv.appendChild(tablo);
+  };
+
+
+
+fetch("https://api.github.com/users").then((res)=> {
+    //console.log(res.json())
+    return res.json()
+}
+).then((data) => {
+
+    tablodaGoster(data,"#tablo" )
+})
